37056e40b5e8fae1330dcce917117e9f35af11a4
Georgi Angelov <the.wizarda@gmail.com>
2021-08-16 19:49:36 +0300

    Add files via upload

--- builder/frameworks/pylink_uploader.py
+++ builder/frameworks/pylink_uploader.py
@@ -3,7 +3,7 @@ from os.path import join
 
 def upload(target, source, env):
    hex  = join(env.get("BUILD_DIR"), env.get("PROGNAME")) + '.hex'
-   serial_no = '000000000'
+   serial_no = '000000000' # TODO
    jlink = pylink.JLink()
    jlink.open()  # serial_no
    jlink.power_on()
@@ -13,5 +13,6 @@ def upload(target, source, env):
    sys.stdout.write('\tCPU Id: 0x%x\n' % jlink.core_cpu())
    sys.stdout.write('\tCore Name: %s\n' % jlink.core_name())
    sys.stdout.write('\tDevice Family: %d\n' % jlink.device_family())   
+   jlink.erase()
    jlink.flash_file(hex, 0x0)
    jlink.reset()
