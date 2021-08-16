16f1cefc1e3b61a3aefbb17dee503a0927f33545
Georgi Angelov <the.wizarda@gmail.com>
2021-08-16 17:08:50 +0300

    Add files via upload

--- builder/frameworks/pylink_uploader.py
+++ builder/frameworks/pylink_uploader.py
@@ -1,11 +1,17 @@
-import pylink
+import pylink, sys
 from os.path import join
 
 def upload(target, source, env):
    hex  = join(env.get("BUILD_DIR"), env.get("PROGNAME")) + '.hex'
    serial_no = '000000000'
    jlink = pylink.JLink()
-   jlink.open()
+   jlink.open()  # serial_no
+   jlink.power_on()
+   #jlink.set_tif(7) # cJTAG not work
    jlink.connect("CC2652R1F", verbose=True)
+   sys.stdout.write('\tARM Id: 0x%x\n' % jlink.core_id())
+   sys.stdout.write('\tCPU Id: 0x%x\n' % jlink.core_cpu())
+   sys.stdout.write('\tCore Name: %s\n' % jlink.core_name())
+   sys.stdout.write('\tDevice Family: %d\n' % jlink.device_family())   
    jlink.flash_file(hex, 0x0)
    jlink.reset()
