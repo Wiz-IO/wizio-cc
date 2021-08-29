35aef94203c4a06abfeea1863288831b1dce52c2
Georgi Angelov <the.wizarda@gmail.com>
2021-08-29 23:56:22 +0300

    FIX server_args !?!?

--- platform.py
+++ platform.py
@@ -56,9 +56,10 @@ class WizioccPlatform(PlatformBase):
             if link in non_debug_protocols or link in debug["tools"]: continue
             #print('LINK', link, 'TARGET', debug.get("openocd_target"))
             server_args = [
-                "-s", "$PACKAGE_DIR/scripts",
-                "-f", "interface/%s.cfg" % 'jlink', # link,
-                "-f", "target/%s" % 'ti_cc26x2.cfg' # debug.get("openocd_target")
+                ### TODO
+                ###"-s", "$PACKAGE_DIR/scripts",
+                ###"-f", "interface/%s.cfg" % 'jlink', # link,
+                ###"-f", "target/%s" % 'ti_cc26x2.cfg' # debug.get("openocd_target")
             ]
             print('SERVER ARG', server_args)
             
