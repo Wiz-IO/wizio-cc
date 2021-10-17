13ab326f1210b3897bed8646f05412bd5641a08b
Georgi Angelov <the.wizarda@gmail.com>
2021-10-17 13:41:14 +0300

    Add files via upload

--- builder/frameworks/freertos-CC2652.py
+++ builder/frameworks/freertos-CC2652.py
@@ -16,7 +16,8 @@ def dev_init(env, platform):
 
     os_path = join( env.framework_dir, 'library', 'freertos')  
 
-    #[INI] board_build.linker = $PROJECT_DIR/custom.ld
+    # CHANGE default linker script
+    #   [INI] board_build.linker = $PROJECT_DIR/custom.ld
     linker = env.BoardConfig().get('build.linker', 'default')
     if 'default' != linker and '$PROJECT_DIR' in linker:
         linker = linker.replace('$PROJECT_DIR', env['PROJECT_DIR']).replace('\\', '/')
@@ -24,11 +25,29 @@ def dev_init(env, platform):
     else:
         env.Append( LDSCRIPT_PATH = join(os_path, 'freertos.ld') )
 
-    # COPY FreeRTOSConfig.h
+    # COPY default FreeRTOSConfig.h to the PROJECT
     templates = join(env.PioPlatform().get_package_dir('framework-wizio-cc'), 'templates')   
     include = join(env.subst('$PROJECT_DIR'), 'include')   
     if False == os.path.isfile( join(include, 'FreeRTOSConfig.h') ): 
         do_copy(templates, include, 'FreeRTOSConfig.h' ) 
+
+    # DPL default is LIB( libdpl_freertos.a ) or compile as:
+    #   [INI] board_build.dpl = src or lib
+    dpl_path = join(os_path, 'dpl')     
+    dpl = env.BoardConfig().get('build.dpl', 'default')
+    if dpl == 'default' and True == os.path.isfile( join(dpl_path, 'libdpl_freertos.a') ):
+        env.Append( LIBPATH = [dpl_path], LIBS = [ 'dpl_freertos', ], ) 
+        print('  * OS-DPL       : Library')
+    elif dpl == 'src':
+        env.BuildSources( join( '$BUILD_DIR', platform, 'dpl' ), join( os_path, 'dpl') ) 
+        print('  * OS-DPL       : compiled as SRC')
+    elif dpl == 'lib':
+        env.libs.append( env.BuildLibrary( join( '$BUILD_DIR', platform, 'dpl_freertos' ), join( os_path, 'dpl') ) )
+        env.Append(LIBS = env.libs)  
+        print('  * OS-DPL       : compiled as LIB')   
+    else:  
+        print('  * OS-DPL       : ERRROR !!! [INI] board_build.dpl = src OR lib')
+        exit(0)
 
     env.Append( 
         CPPDEFINES = [ 'USE_FREERTOS' ],        
@@ -42,11 +61,5 @@ def dev_init(env, platform):
         LINKFLAGS  = [ '--entry=resetISR', ]            
     )         
 
-    env.BuildSources( join( '$BUILD_DIR', platform, 'freertos', 'src' ), join( os_path, 'src') )
-    env.BuildSources( join( '$BUILD_DIR', platform, 'freertos', 'dpl' ), join( os_path, 'dpl') ) 
-    env.BuildSources( join( '$BUILD_DIR', platform, 'freertos', 'sdk' ), join( os_path, 'sdk') )
-
-     
-
-
-
+    env.BuildSources( join( '$BUILD_DIR', platform, 'src' ), join( os_path, 'src') )
+    env.BuildSources( join( '$BUILD_DIR', platform, 'sdk' ), join( os_path, 'sdk') )
